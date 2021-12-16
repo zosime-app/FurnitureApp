bafefdeb880e516829af0a67d41ed6d419e07f66
zosime-app <zosime1025@gmail.com>
2021-12-17 03:19:41 +0900

    add Home screen

---
+++ screens/ItemDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const ItemDetail = () => {
+  return (
+    <View>
+      <Text>ItemDetail</Text>
+    </View>
+  );
+};
+
+export default ItemDetail;
